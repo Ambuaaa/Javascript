da38b590872863b238774041840730424fd5e1b9
Abhishek Kumar <105266096+Ambuaaa@users.noreply.github.com>
2025-05-16 08:11:50 +0000

    operations

--- 01_basics/03_ConversionOrOperation.js
+++ 01_basics/03_ConversionOrOperation.js
@@ -38,3 +38,38 @@ let somenumber = 22
 let StringNumber = String(somenumber)
 
 console.log(typeof StringNumber); // string
+
+// ************Operations **********************
+
+let value = 3 
+let negValue = - value
+console.log(negValue) // -3
+
+// console.log(2+2); 
+// console.log(2-2); 
+// console.log(2*2); 
+// console.log(2**3); // 2 power 3 
+// console.log(2/2); 
+// console.log(2%2); 
+
+let str1 = "abhishek"
+let str2 = " bhai"
+let str3 = str1 + str2 
+console.log(str3); // abhishek bhai 
+
+// The problems come here
+console.log("1" + 2 ); // 12
+console.log(1 + "2"); // 12
+console.log("1" + 2 + 2 ); //122
+console.log(1 + 2 + "2"); // 32 => This is the problem 
+
+// the suggestion is to not write some messy code and please use parenthesis in such situation 
+
+// In JavaScript, if a string comes first in an expression using +, everything after it is treated as a string (string concatenation). 
+// If the string comes last, earlier numbers are evaluated first (number addition), then converted to a string if needed.
+
+ 
+
+
+
+ 
