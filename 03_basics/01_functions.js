b85ebe777d8c6f93b982e20a456403bb5ffa5e8a
Abhishek Kumar <105266096+Ambuaaa@users.noreply.github.com>
2025-06-28 15:28:06 +0000

    revisiting

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -9,27 +9,29 @@ function sayMyName(){
     console.log("K");
 }
 // sayMyName // this is just reference
-// sayMyName() // this is execution
-
+sayMyName() // this is execution
+ 
 function addTwoNumbers(num1 , num2){ // this is parameters (num1 , num2)
     console.log(num1 + num2);
 }
+//// addTwoNumbers() // NaN
 // addTwoNumbers(3,4) // this is arguments 
-// what if we want to store the value of addTwoNumbers in some variable ?
-const result = addTwoNumbers(3,5)
-// console.log(result); // the answer will be undefined
 
-//-----How to do this ----------
+// Q) What if we want to store the value of addTwoNumbers in some variable ?
+const result = addTwoNumbers(3,5)
+// console.log(result); // the answer will be undefined => because the above funtion is not returning anything, it is just printing the value 
+
+//-----------------How to do this -----------------
 
 function addTwoNumbers (nums1 , nums2){
     let result = nums1 + nums2 
     return result 
 }
-// console.log("Result is :" ,  result);
+// console.log("Result is :" ,  result); // Result is : 8
 
-// --------------
+// -----------What else we can do ----------
 function loginUserMessage(username){
-    if(username===undefined){
+    if(username===undefined){ // or you can write - if(!username){}
         console.log("Please enter a username");
         return
     }
@@ -40,14 +42,24 @@ function loginUserMessage(username){
 
 
 
-// ----------REST OPERATOR--------------------------------
+// ------------------REST OPERATOR(Lecture 20)--------------------------
+
 //-----------------multiple prices adding to the shopping cart (using ... operator)-----------
 function calculateCartPrice(...num1){ // ... is also called rest operator and also called spread operator according to the needs. // here called rest operator
     return num1
 }
-console.log(calculateCartPrice(200 , 300 , 400)); // it will convert to array [ 200, 300, 400 ]
+console.log(calculateCartPrice(200 , 300 , 400)); // it will convert to array => [ 200, 300, 400 ]
 
-// ---- using objects via function------
+// --------Can also be writtten as-------
+/*
+function calculateCartPrice(val1 , val2 , ...num1){
+    return num1 
+}
+console.log(calculateCartPrice(200 , 300 , 400 , 2000)); // val1 = 200 , val2 = 300 and the rest goes into ...num1
+ */
+
+
+// -------- using objects via functions-----------
 const user = {
     username: "Abhishek" ,
     price: 199
@@ -57,20 +69,22 @@ function handleObeject(anyObject){
 }
 // handleObeject(user) // Username is Abhishek and the price is 199
 
-// also can do like
-
-// handleObeject({
-//     username: "Abhishek" , 
-//     price: 455
-// })
+//-------also can do like--------
+/*
+handleObeject({
+    username: "Abhishek" , 
+    price: 455
+})
+*/
 
 // ---------arrays in function-------------
 const mynewArray = [200 , 50 , 450 , 600]
+
 function returnSecondValue(getArray){
     return getArray[1]
 }
 // console.log(returnSecondValue(mynewArray)); // 50
-// can also do like
+// ------can also do like--------
 // console.log(returnSecondValue([200,50,100,50])); // 50
 
 
